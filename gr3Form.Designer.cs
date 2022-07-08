0a408d003f7b1e599d219f9db3438ec9baede22a
CR <caleb.rutherford@gmail.com>
2022-07-07 23:34:19 -0400

    idk
    I think I resized the Gr3 form

--- gr3Form.Designer.cs
+++ gr3Form.Designer.cs
@@ -29,12 +29,52 @@ namespace GT7_Randomizer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.carName = new System.Windows.Forms.ColumnHeader();
+            this.drivetrain = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.carName,
+            this.drivetrain});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(781, 385);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // carName
+            // 
+            this.carName.Text = "Car Name";
+            this.carName.Width = 300;
+            // 
+            // drivetrain
+            // 
+            this.drivetrain.Text = "Drivetrain";
+            this.drivetrain.Width = 100;
+            // 
+            // gr3Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "gr3Form";
             this.Text = "gr3Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader carName;
+        private System.Windows.Forms.ColumnHeader drivetrain;
     }
 }
